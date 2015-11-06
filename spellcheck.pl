1025f23689ab8f2414c25e25cdb4da0b014a03c3
Jakub Wilk <jwilk@jwilk.net>
2015-11-06 19:11:46 +0100

    Bump version to 0.7.

--- spellcheck.pl
+++ spellcheck.pl
@@ -18,7 +18,7 @@ use vars qw($VERSION %IRSSI);
 use Irssi 20070804;
 use Text::Aspell;
 
-$VERSION = '0.6.1';
+$VERSION = '0.7';
 %IRSSI = (
     authors     => 'Jakub Wilk, Jakub Jankowski, Gabriel Pettier',
     name        => 'spellcheck',
